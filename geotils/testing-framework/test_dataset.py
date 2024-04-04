d7e1ad6c1439e5b8f6ce925f071d011c2e4f962f
abbassthe <47321004+abbassthe@users.noreply.github.com>
2024-04-04 11:37:46 +0300

    Update test_dataset.py

--- geotils/testing-framework/test_dataset.py
+++ geotils/testing-framework/test_dataset.py
@@ -17,9 +17,7 @@ from torchgeo.datasets import BoundingBox
 
 class TestGetMethod(unittest.TestCase):
     def test_vlm(self):
-        v = vlm(
-            r"C:\Users\abbas\OneDrive\Documents\GitHub\geotils\geotils\testing-framework\assets\data\vlm"
-        )
+        v = vlm(r"testing-framework\assets\data\vlm")
         ans = {
             "id": 0,
             "date_added": 1543570371.6343634,
@@ -308,9 +306,7 @@ class TestGetMethod(unittest.TestCase):
         bb = BoundingBox(
             225986.4361739957, 245486.43617399564, 883271.0247782532, 917771.025, 0, 9
         )
-        mc = MassachusettsBuildingsDataset(
-            r"C:\Users\abbas\OneDrive\Documents\GitHub\geotils\geotils\testing-framework\assets\data\archive"
-        )
+        mc = MassachusettsBuildingsDataset(r"testing-framework\assets\data\archive")
         to_test = to_test = torch.nonzero(
             mc.__getitem__(bb, mask_extension=".tif")["mask"]
         ).numpy()[0]
