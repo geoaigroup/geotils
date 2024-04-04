154b3777fd9dd040cb375fc6fb9007b265c4a60e
abbassthe <47321004+abbassthe@users.noreply.github.com>
2024-04-04 11:54:23 +0300

    fixed paths

--- geotils/testing-framework/test_visualization.py
+++ geotils/testing-framework/test_visualization.py
@@ -11,8 +11,8 @@ import random
 
 
 class TestVisualization(unittest.TestCase):
-    image_filename = r"C:\Users\abbas\OneDrive\Documents\GitHub\geotils\geotils\testing-framework\assets\image.png"
-    mask_filename = r"C:\Users\abbas\OneDrive\Documents\GitHub\geotils\geotils\testing-framework\assets\mask.png"
+    image_filename = r"testing-framework\assets\image.png"
+    mask_filename = r"testing-framework\assets\mask.png"
     im = Image.open(image_filename)
     image = np.array(im)
     ma = Image.open(mask_filename)
